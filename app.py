8a3062804989b5beefea193fb6da5a20764b44cb
Simon Arbiser <simon.arbiser@epita.fr>
2023-12-08 10:37:41 +0100

    cc

--- app.py
+++ app.py
@@ -1,3 +1,42 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0cf5625c9e11c1bed07db2bfdd36a24b9af94f3887cbc330a85a97920ca0fefc
-size 1135
+import numpy as np
+import cv2 as cv
+from PIL import Image, ImageFont, ImageDraw
+from random import randint
+import requests, os
+
+font_size = 28 # px
+KEY = "AIzaSyA4zS6S5OKrWWTOV-9XsYKGTlj0F0hVvcg"
+
+def treatment(path):
+    img = cv.imread(path, cv.IMREAD_GRAYSCALE)
+
+    img = cv.Canny(img, 100, 200)
+    img = cv.resize(img, (28, 28))
+
+
+def dlDigits(path):
+    font = ImageFont.truetype(path, font_size)
+    for i in range(10):
+        try:
+            img = Image.new('RGBA', (font_size, font_size), color = (0, 0, 0))
+            d = ImageDraw.Draw(img)
+            d.text((0, 0), str(i), fill=(255, 255, 255), font=font)
+            img.save(f"./data/{i}/{randint(0, 10000000)}.png")
+        except:
+            pass
+
+def dlFonts(url):
+    r  = requests.get(url)
+    open(f"ttf/{randint(0, 10000000)}.ttf", 'wb').write(r.content)
+
+def discoverFont():
+    r = requests.get(f"https://www.googleapis.com/webfonts/v1/webfonts?key={KEY}")
+    fonts = r.json()["items"]
+    for font in fonts:
+        for f in font["files"]:
+                dlFonts(font["files"][f])
+
+
+#discoverFont()
+for f in os.listdir("ttf"):
+    dlDigits(f"ttf/{f}")
--- cli.c
+++ cli.c
@@ -1,3 +1,87 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:043d873039d4168dc62f379617ebe4593961bcadfc81e49185b63d8319d168f5
-size 2985
+//
+// Created by simon on 12/7/2023.
+//
+#include <stdio.h>
+#include "matrix.h"
+#include "csv.h"
+#include "train.h"
+#include "img.h"
+
+int mainCli() {
+    char* testpath = "handwritten.model";
+
+
+    printf("1) Entrainer le réseau\n");
+    printf("2) Tester le réseau\n");
+    printf("3) Quitter\n");
+
+    int res;
+    scanf("%d", &res);
+    if(res == 1) {
+        matrix** dataset = csvToMatrix("small_train.csv");
+        matrix* imgx = *(dataset + 0);
+        matrix* imgy = *(dataset + 1);
+        imgx->row = 900;
+        imgy->row = 900;
+
+        double* dimgtestx = imgx->data + (imgx->col*900);
+        double* dimgtesty = imgy->data + (imgy->col*900);
+        matrix* imgtestx = init(100, imgx->col, dimgtestx);
+        matrix* imgtesty = init(100, imgy->col, dimgtesty);
+        imgtestx = transpose(imgtestx);
+        imgtesty = transpose(imgtesty);
+        imgtestx = sclalar(imgtestx, (double) 1 / 255);
+        matrix* one_hot_test = one_hot_y(imgtesty);
+
+        imgx = transpose(imgx);
+        imgy = transpose(imgy);
+        imgx = sclalar(imgx, (double) 1 / 255);
+        matrix* one_hot = one_hot_y(imgy);
+
+        matrix** params = descent(imgx, one_hot, 0.1, 300, imgtestx, imgtesty, imgy);
+
+
+
+        save_params(params, testpath);
+        matrix** test_params = load_params(testpath);
+
+        printf("Test du modèle enregistrer\n");
+        test_training(test_params, imgx, imgy);
+        test_training(test_params, imgtestx, imgtesty);
+        return mainCli();
+    } else if(res == 2) {
+        char path[256];
+        printf("renter un chemin vers une image à prédire:\n");
+        scanf("%s", path);
+        matrix** params = load_params(testpath);
+
+        SDL_Surface* surface = paths_to_surface(path, 1);
+        printf("AAA\n");
+        SDL_Surface* ssurface = scale_surfaces(surface, 1);
+        printf("AAA\n");
+        matrix* x = imgs_to_matrix(surface, 1);
+        printf("AAA\n");
+        matrix** layer = foward(x, params);
+        printf("AAA\n");
+        matrix* a2 = *(layer+3);
+        for (int i = 0; i < 10; i++) {
+            printf("%d:  %.2f%%\n", i, *(a2->data + i) * 100);
+        }
+        return mainCli();
+
+    }
+    else if(res == 3) {
+        return 0;
+    } else {
+        return mainCli();
+    }
+}
+
+int main() {
+    printf("    __    ______   ____  ___________ _________   __  __   ____  ______   _   __________  ______  ____  _   _____________\n"
+           "   / /   / ____/  / __ \\/ ____/ ___// ____/   | / / / /  / __ \\/ ____/  / | / / ____/ / / / __ \\/ __ \\/ | / / ____/ ___/\n"
+           "  / /   / __/    / /_/ / __/  \\__ \\/ __/ / /| |/ / / /  / / / / __/    /  |/ / __/ / / / / /_/ / / / /  |/ / __/  \\__ \\ \n"
+           " / /___/ /___   / _, _/ /___ ___/ / /___/ ___ / /_/ /  / /_/ / /___   / /|  / /___/ /_/ / _, _/ /_/ / /|  / /___ ___/ / \n"
+           "/_____/_____/  /_/ |_/_____//____/_____/_/  |_\\____/  /_____/_____/  /_/ |_/_____/\\____/_/ |_|\\____/_/ |_/_____//____/\n");
+    return mainCli();
+}
